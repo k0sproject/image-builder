9fc1739ef071228ac3818cd749f3db9865b28c63
makhov <makhov.alex@gmail.com>
2023-01-31 13:54:54 +0200

    images

---
+++ gen-matrix.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+
+MATRIX_OUTPUT="["
+COMMA=""
+for path in ./images/*; do \
+  IMAGE_NAME=${path#./images/}
+  for version in "$path"/*; do \
+    IMAGE_TAG="$IMAGE_NAME:${version#./images/$IMAGE_NAME/}"
+    MATRIX_OUTPUT+="$COMMA\"$IMAGE_TAG\""
+    COMMA=","
+  done
+done
+MATRIX_OUTPUT+="]"
+
+echo "$MATRIX_OUTPUT"
